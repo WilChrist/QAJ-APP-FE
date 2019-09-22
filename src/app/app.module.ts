d972313d4eaade1131d7260fa7988a997bc1beaa
Willy Nzesseu <willynzesseu@gmail.com>
2019-09-22 17:41:46 +0100

    refactor(app) : define the layout component in the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { LayoutComponent } from './layout/layout.component';
+import { QuotesModule } from './quotes/quotes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
     CoreModule,
+    QuotesModule,
     SharedModule
   ],
   providers: [],
--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -5,13 +5,16 @@ import { QuotesRoutingModule } from './quotes-routing.module';
 import { QuotesComponent } from './quotes.component';
 import { QuotesGridComponent } from './quotes-grid/quotes-grid.component';
 import { QuotesListComponent } from './quotes-list/quotes-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [QuotesComponent, QuotesGridComponent, QuotesListComponent],
   imports: [
     CommonModule,
-    QuotesRoutingModule
-  ]
+    QuotesRoutingModule,
+    SharedModule
+  ],
+  exports: [QuotesComponent]
 })
 export class QuotesModule { }
